858de5f91d9cf4eb952a246b889bbd560333f6df
garyyli <garyli@hanovernorwichschools.org>
2017-11-01 13:56:34 -0400

    Updated from Brython Server: 11/1/2017 1:56:36 PM

--- tictactoe.py
+++ tictactoe.py
@@ -2,11 +2,11 @@
 #11/1/17
 #tictactoe.py
 
-square1 = '+---+'
+
 def printBoard():
     for i in range(1,4):
         print('+---'*3+'+')
         print('|   '*3+'|')
     print('+---'*3+'+')
-    
+
 printBoard()
