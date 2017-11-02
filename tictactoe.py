ce3be0cf15137b0206b15f787a15c182778c1ad3
garyyli <garyli@hanovernorwichschools.org>
2017-11-02 14:24:14 -0400

    Updated from Brython Server: 11/2/2017 2:24:08 PM

--- tictactoe.py
+++ tictactoe.py
@@ -2,11 +2,25 @@
 #11/1/17
 #tictactoe.py
 
+square1
+square2
+square3
+square4
+square5
+square6
+square7
+square8
+square9
 
 def printBoard():
     for i in range(1,4):
         print('+---'*3+'+')
         print('|   '*3+'|')
     print('+---'*3+'+')
+
+def isEmpty(numSquare):
+    if 'X' in numSquare or 'O' in numSquare:
+        return False
+    return True
 
 printBoard()
