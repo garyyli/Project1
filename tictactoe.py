fc43c6925f8498e1dbaab991e225ffb89bf822a5
garyyli <garyli@hanovernorwichschools.org>
2017-11-02 14:43:44 -0400

    Updated from Brython Server: 11/2/2017 2:43:43 PM

--- tictactoe.py
+++ tictactoe.py
@@ -2,25 +2,32 @@
 #11/1/17
 #tictactoe.py
 
-square1
-square2
-square3
-square4
-square5
-square6
-square7
-square8
-square9
+square1 = 1
+square2 = 2
+square3 = 3
+square4 = 4
+square5 = 5
+square6 = 6
+square7 = 7
+square8 = 8
+square9 = 9
 
-def printBoard():
-    for i in range(1,4):
-        print('+---'*3+'+')
-        print('|   '*3+'|')
-    print('+---'*3+'+')
+if __name__ == '__main__':
+    def printBoard():
+        print('_____________')
+        print('|', square1, '|', square2, '|',square3, '|')
+        print('_____________')
+        print('|', square4, '|', square5, '|',square6, '|')
+        print('_____________')
+        print('|', square7, '|', square8, '|',square9, '|')
+        print('_____________')
 
-def isEmpty(numSquare):
-    if 'X' in numSquare or 'O' in numSquare:
-        return False
-    return True
-
-printBoard()
+    def isEmpty(numSquare):
+        if 'X' in numSquare or 'O' in numSquare:
+            return False
+        else:
+            return True
+        
+        
+    isEmpty(int(input('Where would you like to go?')))
+    printBoard()
